994ce5b71ab344f80b2a421e7d6b1061fe63a109
Phạm Quốc Huy <40145089+PhamQuocHuy1101@users.noreply.github.com>
2022-04-16 11:18:43 +0700

    Create bash.sh

---
+++ docker/bash.sh
@@ -0,0 +1,12 @@
+# build image
+docker build https://raw.githubusercontent.com/PhamQuocHuy1101/ops/master/docker/Dockerfile
+
+# push image to docker hub
+docker login
+docker push phamquochuy1101/pytorch-ops:latest
+
+# pull image from docker hub
+docker pull phamquochuy1101/pytorch-ops:latest
+
+# run container
+docker run --rm -it -v $(pwd):/huypq --gpus=all --network=host phamquochuy1101/pytorch-ops
